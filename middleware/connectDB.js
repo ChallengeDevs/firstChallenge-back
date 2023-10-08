4c49e658cf7c95c2ba4754c5d1ba69d75cc341b1
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-10-08 16:59:01 -0300

    feat: Connection to database and pipeline created successfully

---
+++ middleware/connectDB.js
@@ -0,0 +1,15 @@
+const MONGOOSE = require('mongoose');
+
+async function connectBD(req = null, res = null, next = null) {
+  try {
+    await MONGOOSE.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Conectado ao BD!');
+    try { next(); } catch { };
+    return MONGOOSE
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
+module.exports = connectBD;
